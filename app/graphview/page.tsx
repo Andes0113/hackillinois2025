824f50fbbffcf39134eed8b835f4f3c472a8ad87
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2025-03-01 23:34:30 -0600

    Sidebar

--- app/graphview/page.tsx
+++ app/graphview/page.tsx
@@ -27,7 +27,10 @@ export default function graphview() {
         <option value={1825}>5 years</option>
         <option value={9999}>All time</option>
       </select>
-      <FlowCanvas />
+      <div className="flex">
+        <EmailSidebar />
+        <FlowCanvas />
+      </div>
     </>
   );
 }
