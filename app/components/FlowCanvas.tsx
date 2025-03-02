a4d917c57b837ea9ecdee8bfdd5d24d9e07752ce
230pelin <peter.lin2305@gmail.com>
2025-03-01 22:15:05 -0600

    memoized node types, added timeframe dropdown

--- app/components/FlowCanvas.tsx
+++ app/components/FlowCanvas.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import ReactFlow, { Controls, Node, Edge } from "reactflow";
 import "reactflow/dist/style.css";
 import CustomNode from "./CustomNode";
@@ -18,19 +18,19 @@ interface EmailProps {
 
 const FlowDiagram: React.FC = () => {
 
-  const nodeTypes = { custom: CustomNode };
+  const nodeTypes = useMemo(() => ({ "custom": CustomNode }), []);
   const [sharedPopupState, setSharedPopupState] = useState<boolean>(false);
   const [selectedEmail, setSelectedEmail] = useState<EmailProps | null>(null);
-  const updateSharedPopupState = (email: EmailProps, state : boolean) => {
+  const updateSharedPopupState = (email: EmailProps, state: boolean) => {
     console.log("updated state to ", state);
     setSharedPopupState(state);
     setSelectedEmail(email);
   };
 
   const emailData: EmailProps[] = [
-    { sender: "person1", receiver: "person2", subject: "subject1", message: "body1", dateSent: "2023-10-01", daysAgo: 1},
-    { sender: "person3", receiver: "person4", subject: "subject2", message: "body2", dateSent: "2023-10-02", daysAgo: 10},
-    { sender: "person5", receiver: "person6", subject: "subject3", message: "body3", dateSent: "2023-10-03", daysAgo: 20},
+    { sender: "person1", receiver: "person2", subject: "subject1", message: "body1", dateSent: "2023-10-01", daysAgo: 1 },
+    { sender: "person3", receiver: "person4", subject: "subject2", message: "body2", dateSent: "2023-10-02", daysAgo: 10 },
+    { sender: "person5", receiver: "person6", subject: "subject3", message: "body3", dateSent: "2023-10-03", daysAgo: 20 },
   ]
 
   const initialNodes: Node[] = Array.from(
@@ -55,7 +55,7 @@ const FlowDiagram: React.FC = () => {
           <Controls />
         </ReactFlow>
       </div>
-      {sharedPopupState && selectedEmail != null && <EmailPopup 
+      {sharedPopupState && selectedEmail != null && <EmailPopup
         onClose={() => setSharedPopupState(false)}
         sender={selectedEmail.sender}
         receiver={selectedEmail.receiver}
